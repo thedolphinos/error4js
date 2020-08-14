d8393fb7c84c8eb67c23b544b625c6ef51c1b067
Onur Çipe <onur@dolphinos.com>
2020-08-14 22:02:50 +0300

    feature (db): more than 1 document found

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const FileNotExistError = require("./lib/core/FileNotExistError");
 const DirectoryNotExistError = require("./lib/core/DirectoryNotExistError");
 const StaticClassInstantiationError = require("./lib/class/StaticClassInstantiationError");
 const DocumentNotFoundError = require("./lib/db/DocumentNotFoundError");
+const MoreThan1DocumentFoundError = require("./lib/db/MoreThan1DocumentFoundError");
 
 module.exports = {
   InvalidArgumentsError,
   FileNotExistError,
   DirectoryNotExistError,
   StaticClassInstantiationError,
-  DocumentNotFoundError
+  MoreThan1DocumentFoundError
 };
