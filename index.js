dba4a98a2164fb4678a720c556454a3ccdbbbabe
Onur Çipe <onur@dolphinos.com>
2020-08-14 21:20:20 +0300

    feature (db): document not found

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const InvalidArgumentsError = require("./lib/core/InvalidArgumentsError");
 const FileNotExistError = require("./lib/core/FileNotExistError");
 const DirectoryNotExistError = require("./lib/core/DirectoryNotExistError");
 const StaticClassInstantiationError = require("./lib/class/StaticClassInstantiationError");
+const DocumentNotFound = require("./lib/db/DocumentNotFound");
 
 module.exports = {
   InvalidArgumentsError,
   FileNotExistError,
   DirectoryNotExistError,
-  StaticClassInstantiationError
+  StaticClassInstantiationError,
+  DocumentNotFound
 };
