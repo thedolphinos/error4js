be58d778b4451e7acdd6314c2caa12f034285875
Onur Çipe <onur@dolphinos.com>
2020-08-18 15:51:20 +0300

    feature (http): headers missing, path parameters missing, path parameter missing, query string missing, query string parameter missing

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const StaticClassInstantiationError = require("./lib/class/StaticClassInstantiat
 const DocumentNotFoundError = require("./lib/db/DocumentNotFoundError");
 const MoreThan1DocumentFoundError = require("./lib/db/MoreThan1DocumentFoundError");
 const HTTPError = require("./lib/http/HTTPError");
+const HeadersMissingError = require("./lib/http/client/HeadersMissingError");
+const PathParametersMissingError = require("./lib/http/client/PathParametersMissingError");
+const PathParameterMissingError = require("./lib/http/client/PathParameterMissingError");
+const QueryStringMissingError = require("./lib/http/client/QueryStringMissingError");
+const QueryStringParameterMissingError = require("./lib/http/client/QueryStringParameterMissingError");
 const BodyMissingError = require("./lib/http/client/BodyMissingError");
 const InternalServerError = require("./lib/http/server/InternalServerError");
 
@@ -18,6 +23,11 @@ module.exports = {
   DocumentNotFoundError,
   MoreThan1DocumentFoundError,
   HTTPError,
+  HeadersMissingError,
+  PathParametersMissingError,
+  PathParameterMissingError,
+  QueryStringMissingError,
+  QueryStringParameterMissingError,
   BodyMissingError,
   InternalServerError
 };
