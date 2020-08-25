a06c0cec8fcd57d3aefde50627818223adb27cda
Onur Çipe <onur@dolphinos.com>
2020-08-25 22:31:51 +0300

    fix: add missing exports

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 "use strict";
 
+const DeveloperError = require("./lib/core/DeveloperError");
 const InvalidArgumentsError = require("./lib/developer/InvalidArgumentsError");
+const StaticClassInstantiationError = require("./lib/developer/StaticClassInstantiationError");
 const FileNotExistError = require("./lib/developer/FileNotExistError");
 const DirectoryNotExistError = require("./lib/developer/DirectoryNotExistError");
-const StaticClassInstantiationError = require("./lib/developer/StaticClassInstantiationError");
+
+const DbError = require("./lib/core/DbError");
 const DocumentNotFoundError = require("./lib/db/DocumentNotFoundError");
 const MoreThan1DocumentFoundError = require("./lib/db/MoreThan1DocumentFoundError");
+
 const HTTPError = require("./lib/core/HTTPError");
+
+const ServerError = require("./lib/http/ServerError");
+const InternalServerError = require("./lib/http/server/InternalServerError");
+
+const ClientError = require("./lib/http/ClientError");
 const BadRequestError = require("./lib/http/client/BadRequestError");
 const HeadersMissingError = require("./lib/http/client/bad-requests/HeadersMissingError");
 const PathParametersMissingError = require("./lib/http/client/bad-requests/PathParametersMissingError");
@@ -14,27 +23,29 @@ const PathParameterMissingError = require("./lib/http/client/bad-requests/PathPa
 const QueryStringMissingError = require("./lib/http/client/bad-requests/QueryStringMissingError");
 const QueryStringParameterMissingError = require("./lib/http/client/bad-requests/QueryStringParameterMissingError");
 const BodyMissingError = require("./lib/http/client/bad-requests/BodyMissingError");
-const InternalServerError = require("./lib/http/server/InternalServerError");
 
 module.exports = {
+  DeveloperError,
   InvalidArgumentsError,
+  StaticClassInstantiationError,
   FileNotExistError,
   DirectoryNotExistError,
 
-  StaticClassInstantiationError,
-
+  DbError,
   DocumentNotFoundError,
   MoreThan1DocumentFoundError,
 
   HTTPError,
 
-  BadRequestError,
+  ServerError,
+  InternalServerError,
 
+  ClientError,
+  BadRequestError,
   HeadersMissingError,
   PathParametersMissingError,
   PathParameterMissingError,
   QueryStringMissingError,
   QueryStringParameterMissingError,
-  BodyMissingError,
-  InternalServerError
+  BodyMissingError
 };
